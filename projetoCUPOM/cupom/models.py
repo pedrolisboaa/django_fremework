e68a4347aea915a33250f06fb7d2648ecc8e100f
pedrolisboaa <phlisboa2000@gmail.com>
2023-08-23 17:20:14 -0300

    subindo o projeto de cupons

--- projetoCUPOM/cupom/models.py
+++ projetoCUPOM/cupom/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Base(models.Model):
+    criado = models.DateField('Data de Criação',  auto_now_add=True)
+    modificado = models.DateField('Data de modificação', auto_now=True)
+    
+    class Meta:
+        abstract = True
+
+
+class Cupons(Base):
+    cpf = models.CharField('CPF', max_length=14)
+    numero_cupom = models.CharField('Numero Cupom', max_length=5, unique=True)
+
+    def __str__(self):
+        return self.cpf
