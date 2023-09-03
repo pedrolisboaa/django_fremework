d9fcf0e28195547999d2d7a0ee6ccccd461063b7
pedrolisboaa <phlisboa2000@gmail.com>
2023-09-03 20:18:27 -0300

    retirando um projeto, pois esta solto

--- projetoCUPOM/cupom/static/js/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('btn-cpf').addEventListener('submit', function(evento){
-    let cpfInput = document.getElementById('btn-cpf')
-    let limpandoCpf = cpfInput.value.replace(/[.-]/g, '')
-    cpfInput.value = limpandoCpf
-})
-
-console.log('oi!')
